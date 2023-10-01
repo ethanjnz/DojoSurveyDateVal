2d8b3b3ae62b18378a9547ad29802ac2c9c06029
Ethan Jones <ethan08jones@gmail.com>
2023-10-01 18:20:30 -0400

    finished Dojo Suvery Form MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dojoSurveyClone.Models;
+
+namespace dojoSurveyClone.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public static User FakeUserDb = new();
+
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(User NewUser)
+    {
+        FakeUserDb = NewUser;
+        return RedirectToAction("Details");
+    }
+
+    [HttpGet("details")]
+    public IActionResult Details()
+    {
+
+        return View("Details", FakeUserDb);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dojoSurveyClone.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
